a7adebb2de5e130886cfeea64e67cd910aa402f4
Dmitry Bessonov <dmitrybessonov@icloud.com>
2020-02-03 11:35:41 +0300

    minor improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
-import {BrowserRouter as Router} from "react-router-dom";
+// import {BrowserRouter as Router} from "react-router-dom";
 
 import "./App.css";
 
--- src/components/pages/Page404.jsx
+++ src/components/pages/Page404.jsx
@@ -1,39 +1,51 @@
-import React from "react";
-import {HashLink as Link} from 'react-router-hash-link';
+import React, {Component} from "react";
+import { HashLink as Link } from "react-router-hash-link";
 import styled from "styled-components";
 
-const Page404 = () => {
-  const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding-top: 100px;
-    width: 100%;
-    height: calc(100vh - 150px);
-  `;
-  const Text = styled.h1`
-    color: #888;
-    margin: 0 auto;
-    text-align: center;
-    animation-duration: 1s;
-    animation-name: showPic;  
-  `;
-  const BackToHome = styled.h2`
-    color: #444;
-    margin: 0 auto;
-    text-align: center;
-    animation-duration: 1s;
-    animation-name: showPic;  
-  `;
+class Page404 extends Component {
 
-  return (
-    <Wrapper>
-      <Text>This page will appear soon..</Text> 
-      <br/>
-      <Link to='/'><BackToHome>Back to the main page</BackToHome></Link>
-    </Wrapper>
-  );
-};
+  componentDidMount = () => {
+    window.scrollTo(0, 0);
+  };
+
+  render() {
+    
+    // const Page404 = () => {
+
+    const Wrapper = styled.div`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding-top: 100px;
+      width: 100%;
+      height: calc(100vh - 150px);
+    `;
+    const Text = styled.h1`
+      color: #888;
+      margin: 0 auto;
+      text-align: center;
+      animation-duration: 1s;
+      animation-name: showPic;
+    `;
+    const BackToHome = styled.h2`
+      color: #444;
+      margin: 0 auto;
+      text-align: center;
+      animation-duration: 1s;
+      animation-name: showPic;
+    `;
+
+    return (
+      <Wrapper>
+        <Text>This page will appear soon..</Text>
+        <br />
+        <Link to="/">
+          <BackToHome>Back to the main page</BackToHome>
+        </Link>
+      </Wrapper>
+    );
+  }
+}
 
 export default Page404;
